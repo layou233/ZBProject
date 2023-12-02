191a7d457818cb9f3570197ef707f3dd94d88de6
layou233 <admin@launium.com>
2023-12-02 23:18:51 +0800

    New homepage & build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+# install hugo
+CGO_ENABLED=1 GOAMD64=v3 go install -v -trimpath -ldflags="-s -w -buildid=" -tags extended github.com/gohugoio/hugo@latest
+
+# build home
+cd home
+hugo
+mv public ../public
+
+# build doc
+npm i -g gitbook-cli && gitbook build ./docs ./public/doc
